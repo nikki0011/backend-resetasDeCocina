c5b354c076df52cf4b35df57a25d4bdfc3211be7
Nair Paez <nairpaez@outlook.com>
2026-08-01 16:04:49 -0300

    creamos route.use usuarios en index.route

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import recetasRouter from "./recetas.routes.js"
+import usuariosRouter from "./usuarios.routes.js"
 // http://localhost:3000/api/recetas/
+// http://localhost:3000/api/usuariosRecetas/
 
 const router = Router()
 
 router.use('/recetas', recetasRouter)
+router.use('/usuarios-recetas', usuariosRouter)
 
 export default router
