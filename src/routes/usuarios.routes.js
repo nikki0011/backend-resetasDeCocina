bdff61d9ad355f3114bd694619702d54af2193fc
Nair Paez <nairpaez@outlook.com>
2026-08-01 17:01:39 -0300

    agragamos middlewares de validacion en rutas de usuarios

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { borrarUsuarioPorID, crearUsuario, editarUsuarioPorID, listarUsuarios, obtenerUsuariosPorID } from "../controllers/usuarios.controllers.js";
+import { validacionIDUsuario, validacionUsuario, validacionUsuarioPatch } from "../middlewares/validacionUsuario.js";
 
 const router = Router()
 // http://localhost:3000/api/usuarios/
 
-router.route('/').post(crearUsuario).get(listarUsuarios)
-router.route('/:id').get( obtenerUsuariosPorID).delete( borrarUsuarioPorID).put(editarUsuarioPorID).patch(editarUsuarioPorID)
+router.route('/').post(validacionUsuario,crearUsuario).get(listarUsuarios)
+router.route('/:id').get( validacionIDUsuario,obtenerUsuariosPorID).delete(validacionIDUsuario, borrarUsuarioPorID).put([validacionIDUsuario,validacionUsuario],editarUsuarioPorID).patch(validacionUsuarioPatch,editarUsuarioPorID)
 
 export default router
