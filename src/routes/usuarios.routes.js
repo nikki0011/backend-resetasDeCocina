99b151ab397128c6aa540a626f923a87f28d66e4
Nair Paez <nairpaez@outlook.com>
2026-08-02 18:22:56 -0300

    agragamos middlewares autententicador y es admin en rutas correnpondiente en ruta usuarios

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { confirmarCodigoVerificacion, crearUsuario, listarUsuarios, login, logout, obtenerPerfil, registrarUsuario, solicitarNuevoCodigo } from "../controllers/usuarios.controllers.js";
+import { autenticador, esAdmin } from "../middlewares/authMiddlewares.js";
 
 
 const router = Router()
 // http://localhost:3000/api/usuarios/
 
-router.route('/').post(crearUsuario).get(listarUsuarios)
+router.route('/').post(crearUsuario).get([autenticador, esAdmin],listarUsuarios)
 router.route('/registro').post(registrarUsuario)
 router.route('/verificar-cuenta').post(confirmarCodigoVerificacion)
 router.route('/reenviar-codigo').post(solicitarNuevoCodigo)
@@ -13,6 +14,6 @@ router.route('/login').post(login)
 router.route('/logout').post(logout)
 
 //ruta provada
-router.route("/perfil").get(obtenerPerfil)
+router.route("/perfil").get(autenticador,obtenerPerfil)
 
 export default router
