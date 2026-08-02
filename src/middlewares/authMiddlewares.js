36b3c53748ef494abc6229e22749a101c4889a6d
Nair Paez <nairpaez@outlook.com>
2026-08-02 18:17:51 -0300

    creamos middlewares autenticador y esAdmin para acceso a perfil

---
+++ src/middlewares/authMiddlewares.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+
+export const autenticador = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    // hay un token
+    if (!token) {
+      return res
+        .status(401)
+        .json({ mensaje: "Acceso no autorizado, token faltante" });
+    }
+    //verificar el token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    //GUARDAR LOS DATOS EN EL REQUEST
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(403).json({ mensaje: "Token invalido o expirado" });
+  }
+};
+
+export const esAdmin = (req, res, next) => {
+  // verificar si tenemos los datos del usuario em el req y verificar si es admin
+  if (!req.user || req.user.rol !== "Admin") {
+    return status(403).json({
+      mensaje: "Acceso denegado: permisos insuficientes",
+    });
+  }
+  next();
+};
