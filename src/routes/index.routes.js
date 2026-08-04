fc4516dcc9aa3200dbc6b721e2a8dbbb34c28fc9
Nair Paez <nairpaez@outlook.com>
2026-08-04 17:00:27 -0300

    agregamos rutas de categorias y ruta categorias a index routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import recetasRouter from "./recetas.routes.js"
 import usuariosRouter from "./usuarios.routes.js"
+import categoriasRouter from "./categorias.routes.js"
 // http://localhost:3000/api/recetas/
 // http://localhost:3000/api/usuarios/
 
@@ -8,5 +9,6 @@ const router = Router()
 
 router.use('/recetas', recetasRouter)
 router.use('/usuarios', usuariosRouter)
+router.use('/categorias', categoriasRouter)
 
 export default router
