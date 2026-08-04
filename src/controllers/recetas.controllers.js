e7e249c7c63a0836657e414fb3b3eb5ba0d0ff87
Nair Paez <nairpaez@outlook.com>
2026-08-04 16:51:08 -0300

    agregamos metodo populate('categoria',nombre descripcon ')en controlador servicio en funciones listar y obtener servicio

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -15,7 +15,7 @@ export const crearReceta = async (req, res) => {
 
 export const listarRecetas = async (req, res) => {
   try {
-    const recetas = await Receta.find();
+    const recetas = await Receta.find().populate('categoria','nombre descripcion');
     res.status(200).json(recetas);
   } catch (error) {
     console.error(error);
@@ -28,7 +28,7 @@ export const listarRecetas = async (req, res) => {
 export const obtenerRecetaPorID = async (req, res) => {
   try {
     console.log(req.params.id);
-    const recetaBuscada = await Receta.findById(req.params.id);
+    const recetaBuscada = await Receta.findById(req.params.id).populate('categoria','nombre descripcion');;
     if (!recetaBuscada) {
       return res
         .status(404)
