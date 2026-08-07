5f14f83dd434733199e436e31a55c6ab3e25e1c4
Nair Paez <nairpaez@outlook.com>
2026-08-07 11:05:59 -0300

    corrijimos error de importancion de routerindex faltante en archivo index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import router from "./src/routes/index.routes.js";
 import Server from "./src/server/config.js";
 
 // Instanciar la clase server
 const server = new Server()
-
+server.app.use('/api', router)
 
 server.listen()
